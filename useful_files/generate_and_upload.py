62f1c8833cfb8cb64ca1ce7b1355898eae4644b5
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-05-17 17:52:05 +0200

    fixed typos in generate and upload script

--- useful_files/generate_and_upload.py
+++ useful_files/generate_and_upload.py
@@ -25,21 +25,24 @@ ANNOTATION_NAME = sys.argv[2]
 
 if CLEAN_ANNOTATIONS:
     ANNOTATION_NAME_CLEANED = ANNOTATION_NAME[:-4] + '_cleaned.xml'
-    call(['python', 'map_annotation_tool_to_btl_converter.py', ANNOTATION_NAME, ANNOTATION_NAME_CLEANED])
+    call(['python', '../KnowledgeBase/map_annotation_tool_to_btl_converter.py', ANNOTATION_NAME, ANNOTATION_NAME_CLEANED])
 
 
 # start mongo deamon
 mongod_process = MongoDProcess()
+mongod_process.start()
 
 # execute generate example data and wait for it to write everything to the database files
-call(['python', 'generate_example_data_clf.py', DATABASE_NAME,  ANNOTATION_NAME])
+call(['python', '../KnowledgeBase/generate_example_data_clf.py', DATABASE_NAME,  ANNOTATION_NAME])
 time.sleep(3)
 
 # terminate the daemon
 mongod_process.terminate()
 
 # copy the database to cadmium
-call(['scp', DB_PATH + DATABASE_NAME + '.*', 'sync0r@cadmium:/vol/pepper/systems/pepper-robocup-nightly/opt/knowledgebase-current/'])
+call(['scp', DB_PATH + DATABASE_NAME + '.0', 'sync0r@cadmium:/vol/pepper/systems/pepper-robocup-nightly/opt/knowledgebase-current'])
+call(['scp', DB_PATH + DATABASE_NAME + '.ns', 'sync0r@cadmium:/vol/pepper/systems/pepper-robocup-nightly/opt/knowledgebase-current'])
 
 # remove evidence all evidence
 call(['rm', '-r', DB_PATH])
+call(['rm', 'mongodb.log'])
