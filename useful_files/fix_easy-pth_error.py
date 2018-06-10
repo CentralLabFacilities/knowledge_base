bcc782fc1ac46033976a2b07239825834f84a56c
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-06-10 16:51:19 +0200

    check for file existing before changing

--- useful_files/fix_easy-pth_error.py
+++ useful_files/fix_easy-pth_error.py
@@ -1,20 +1,28 @@
 import fileinput
 import sys
 
+from pathlib2 import Path
+
+
 prefix = sys.argv[1]
 
 filename = prefix + "/lib/python2.7/dist-packages/easy-install.pth"
 filename = filename.replace("//", "/")
-print("removing /usr/lib/python2.7 from " + filename)
-for line in fileinput.input(filename, inplace=True):
-    newline = line.replace("\n", "")
-    if not '/usr/lib/python2.7' in newline:
-        print(newline)
+
+file = Path(filename)
+if file.is_file():
+    print("removing /usr/lib/python2.7 from " + filename)
+    for line in fileinput.input(filename, inplace=True):
+        newline = line.replace("\n", "")
+        if not '/usr/lib/python2.7' in newline:
+            print(newline)
 
 filename = prefix + "/lib/python2.7/site-packages/easy-install.pth"
 filename = filename.replace("//", "/")
-print("removing /usr/lib/python2.7 from " + filename)
-for line in fileinput.input(filename, inplace=True):
-    newline = line.replace("\n", "")
-    if not '/usr/lib/python2.7' in newline:
-        print(newline)
+file = Path(filename)
+if file.is_file():
+    print("removing /usr/lib/python2.7 from " + filename)
+    for line in fileinput.input(filename, inplace=True):
+        newline = line.replace("\n", "")
+        if not '/usr/lib/python2.7' in newline:
+            print(newline)
