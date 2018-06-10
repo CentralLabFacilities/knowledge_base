d540a69ace4e573938fa33bd7bdfd26d9a8ead18
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-06-10 16:40:13 +0200

    added debug output for easy install pth fix file

--- useful_files/fix_easy-pth_error.py
+++ useful_files/fix_easy-pth_error.py
@@ -2,12 +2,14 @@ import fileinput
 import sys
 
 prefix = sys.argv[1]
+print("removing /usr/lib/python2.7 from prefix \"" + prefix + "\" easy-install.pth")
 
 for line in fileinput.input(prefix + "/lib/python2.7/dist-packages/easy-install.pth", inplace=True):
     newline = line.replace("\n", "")
-    if not '/usr' in newline:
+    if not '/usr/lib/python2.7' in newline:
         print(newline)
+
 for line in fileinput.input(prefix + "/lib/python2.7/site-packages/easy-install.pth", inplace=True):
     newline = line.replace("\n", "")
-    if not '/usr' in newline:
+    if not '/usr/lib/python2.7' in newline:
         print(newline)
