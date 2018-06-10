97fabc9cd0f7cbbd9047a3d51b1b7f95e6df05e9
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2018-06-10 16:30:16 +0200

    fixed a bug with the newly added pth script

--- useful_files/fix_easy-pth_error.py
+++ useful_files/fix_easy-pth_error.py
@@ -1,11 +1,13 @@
 import fileinput
-import os
+import sys
 
-for line in fileinput.input(os.environ.get('prefix') + "/lib/python2.7/dist-packages/easy-install.pth", inplace=True):
+prefix = sys.argv[1]
+
+for line in fileinput.input(prefix + "/lib/python2.7/dist-packages/easy-install.pth", inplace=True):
     newline = line.replace("\n", "")
     if not '/usr' in newline:
         print(newline)
-for line in fileinput.input(os.environ.get('prefix') + "/lib/python2.7/site-packages/easy-install.pth", inplace=True):
+for line in fileinput.input(prefix + "/lib/python2.7/site-packages/easy-install.pth", inplace=True):
     newline = line.replace("\n", "")
     if not '/usr' in newline:
         print(newline)
