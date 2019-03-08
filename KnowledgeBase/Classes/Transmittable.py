8b64d570b8016baa014de003f7a51fe52b80575b
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-03-08 18:09:45 +0100

    Revert "Revert "abandoning nice abstract classes and oop; going for simple error raising instead. why make complex abstraction when simple thing do trick?""
    This reverts commit e642e1fa8d700526954c1c24b3eef6009adf39fb.

--- KnowledgeBase/Classes/Transmittable.py
+++ KnowledgeBase/Classes/Transmittable.py
@@ -1,26 +1,15 @@
-from abc import abstractmethod, ABCMeta
-
-
-class abstractclassmethod(classmethod):
-
-    __isabstractmethod__ = True
-
-    def __init__(self, callable):
-        callable.__isabstractmethod__ = True
-        super(abstractclassmethod, self).__init__(callable)
+from abc import abstractmethod
 
 
 class Transmittable():
-
-    __metaclass__ = ABCMeta
 
     @abstractmethod
     def to_xml(self):
         pass
 
-    @abstractclassmethod
+    @classmethod
     def from_xml(cls, xml_tree):
-        pass
+        raise NotImplementedError
 
     @classmethod
     def from_xml_local(cls, xml_tree):
